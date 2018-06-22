870950fac3b26a935f7ea584e4b19a4aee475696
chandu <chandrasr.palle@gmail.com>
2018-06-22 15:14:27 +0200

    created data using model.create

--- cats.js
+++ cats.js
@@ -9,18 +9,39 @@ var catSchema = new mongoose.Schema({
 
 var Cat = mongoose.model("Cat", catSchema);
 
-var tommy = new Cat({
-        name: "Tommy",
-        age: 3,
-        temperament: "Funny"
-});
+// var tommy = new Cat({
+//         name: "Tommy",
+//         age: 3,
+//         temperament: "Funny"
+// });
 
-tommy.save(function(err, cat){
-    if(err){
-        console.log("There is Something Wrong");
-        console.log(err);
-    }else{
-        console.log(cat);
+// tommy.save(function(err, cat){
+//     if(err){
+//         console.log("There is Something Wrong");
+//         console.log(err);
+//     }else{
+//         console.log(cat);
 
+//     }
+// });
+Cat.create({name: "Amanda",
+age:3,
+temperament:"Loveable"},function(err, cat){
+    if(err)
+    {
+        console.log("Error in saving FIle");
     }
+    else{
+        console.log("SAVED CAT TO DB");
+        console.log(cat);
+    }
+});
+Cat.find({}, function(err, cat){
+    if(err){
+        console.log("Error ", err);
+    }
+    else{
+        console.log(cat);
+    }
+
 });
